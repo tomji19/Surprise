013eaba2dd97da61e75ce4c4c253440cd82f5a4d
YoussefAshour <youssefashour19@gmail.com>
2025-03-10 00:45:43 +0200

    Set base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Surprise/", // Adjust to your Vercel deployment path, if needed
   plugins: [react()],
-  base: '/Surprise/', // Must match your repository name with slashes
-})
+});
