6616b6a02b32f278af4ffc04121c489803c25d8c
YoussefAshour <youssefashour19@gmail.com>
2025-03-10 00:53:38 +0200

    Fix script path for Vite build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Surprise/", // Adjust to your Vercel deployment path, if needed
+  base: "/", // Should be "/" for surprise-lilac.vercel.app root
   plugins: [react()],
 });
